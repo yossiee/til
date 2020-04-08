51f4e76bb63ea8d18f532fd56147eec5a20a97f9
yossiee <38056766+yossiee@users.noreply.github.com>
2020-04-08 23:48:00 +0900

    Learn about terraform basic syntax

---
+++ Infrastructure/terraform/basic-syntax/data.tf
@@ -0,0 +1,20 @@
+# data source
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64_gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_instance.recent_amazon_linux_2.image_id
+  instance_type = "t3.micro"
+}
